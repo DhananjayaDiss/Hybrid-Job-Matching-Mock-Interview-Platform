0a0faf15a53a7db543f0b16af6571971ea892f45
Dhananjaya Dissanayake <dhananjayadissanayake01@gmail.com>
2025-07-27 12:40:00 +0530

    updates

--- run.py
+++ run.py
@@ -2,9 +2,12 @@ import os
 from app import create_app
 from app.auth.routes import init_oauth
 
+# Create app globally so gunicorn can find it
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+init_oauth(app)
+
 def main():
-    app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-    init_oauth(app)
+    # Only used in local development
     app.run(debug=True)
 
 if __name__ == '__main__':
